d1fecd6f2a493b58b5f4adf5366e6ca0948d8231
jack <jack.ye@woheng-official.com>
2024-01-19 17:46:05 +0800

    調整

--- pages/about.vue
+++ pages/about.vue
@@ -3,7 +3,10 @@
     <div class="flex flex-col items-center">
       <h1 class="text-6xl font-semibold text-yellow-400">大家好！我是 Ryan</h1>
       <p class="my-8 text-3xl text-gray-600">這裡是 /about</p>
+      <p>{{ count }}</p>
+      <button @click="increment">+1</button>
     </div>
     <NuxtLink to="/">home</NuxtLink>
   </div>
 </template>
+<script setup></script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,17 @@
     <div class="flex flex-col items-center">
       <h1 class="text-6xl font-semibold text-gray-800">這裡是首頁</h1>
       <NuxtLink to="/about">前往 About</NuxtLink>
+      <p>{{ count }}</p>
+      <button @click="increment">+1</button>
       <!-- <NuxtLink :to="{ name: 'user', params: { id: 1 } }">前往 user</NuxtLink> -->
       <!-- <p @click="fn">go user</p> -->
     </div>
   </div>
 </template>
-<script lang="ts" setup>
+<script setup>
 // const router = useRouter()
-// function fn(): void {
-//   router.push({ name: 'user', params: { id: 1 } })
-// }
+// router.push({ name: 'user-id', params: { id: 1 }, query: { id: 1 } })
+if (process.client) {
+  console.log(localStorage)
+}
 </script>
